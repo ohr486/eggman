b225293be27277cec41a2f5154ffff958c5b20d4
ohr486 <ohr486@gmail.com>
2019-03-11 23:19:07 +0900

    add hex

--- mix.exs
+++ mix.exs
@@ -21,8 +21,12 @@ defmodule Eggman.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_aws, "~> 2.1"},
+      {:ex_aws_cloudformation, "~> 2.0"},
+      {:ex_aws_s3, "~> 2.0"},
+      {:poison, "~> 3.0"},
+      {:hackney, "~> 1.9"},
+      {:sweet_xml, "~> 0.6"},
     ]
   end
 end
