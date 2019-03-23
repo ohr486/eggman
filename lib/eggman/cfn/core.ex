8784f4ea08695464de1d6f48c19a0deff3b72a98
ohr486 <ohr486@gmail.com>
2019-03-23 20:02:39 +0900

    enable blank filter

--- lib/eggman/cfn/core.ex
+++ lib/eggman/cfn/core.ex
@@ -2,12 +2,19 @@ defmodule Eggman.Cfn.Core do
   alias ExAws.Cloudformation
 
   @doc false
+  def list_stacks(stack_status, ""), do: list_stacks_base(stack_status)
+  def list_stacks(stack_status, nil), do: list_stacks_base(stack_status)
   def list_stacks(stack_status, keyword) do
+    list_stacks_base(stack_status)
+    |> Enum.filter(&String.contains?(&1, keyword))
+  end
+
+  @doc false
+  defp list_stacks_base(stack_status) do
     Cloudformation.list_stacks([stack_status_filters: [stack_status]])
     |> ExAws.request!
     |> Map.get(:body)
     |> Map.get(:stacks)
     |> Enum.map(&(Map.get(&1, :name)))
-    |> Enum.filter(&String.contains?(&1, keyword))
   end
 end
