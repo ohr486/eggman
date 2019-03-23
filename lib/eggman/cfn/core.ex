2e547acd77a76482c7dc9d0b0c5319a91aeb8610
ohr486 <ohr486@gmail.com>
2019-03-23 19:28:06 +0900

    add filter list for cfn

--- lib/eggman/cfn/core.ex
+++ lib/eggman/cfn/core.ex
@@ -2,11 +2,12 @@ defmodule Eggman.Cfn.Core do
   alias ExAws.Cloudformation
 
   @doc false
-  def list_stacks(stack_status) do
+  def list_stacks(stack_status, keyword) do
     Cloudformation.list_stacks([stack_status_filters: [stack_status]])
     |> ExAws.request!
     |> Map.get(:body)
     |> Map.get(:stacks)
     |> Enum.map(&(Map.get(&1, :name)))
+    |> Enum.filter(&String.contains?(&1, keyword))
   end
 end
--- lib/mix/tasks/egg.status.ex
+++ lib/mix/tasks/egg.status.ex
@@ -12,12 +12,13 @@ defmodule Mix.Tasks.Egg.Status do
   @doc false
   def run(args) do
     Application.ensure_all_started(:hackney)
-    {options, _argv, _errors} = OptionParser.parse(args, strict: [debug: :boolean, cfn: :string])
-    cfn_list_status
+    {options, _argv, _errors} = OptionParser.parse(args, strict: [debug: :boolean, filter: :string])
+    keyword = Keyword.get(options, :filter)
+    cfn_list_status(keyword)
   end
 
   @doc false
-  defp cfn_list_status do
+  defp cfn_list_status(keyword) do
     Mix.shell.info ""
     Mix.shell.info "Cloudformation Stacks:"
     Mix.shell.info "----------------------"
@@ -28,14 +29,14 @@ defmodule Mix.Tasks.Egg.Status do
       :rollback_complete,
       :delete_complete,
     ]
-    |> Enum.each(&show_stacks(&1))
+    |> Enum.each(&show_stacks(&1, keyword))
     Mix.shell.info "----------------------"
     Mix.shell.info ""
   end
 
   @doc false
-  defp show_stacks(stack_status) do
-    Eggman.Cfn.Core.list_stacks(stack_status)
+  defp show_stacks(stack_status, keyword) do
+    Eggman.Cfn.Core.list_stacks(stack_status, keyword)
     |> Enum.each(&(Mix.shell.info("[#{stack_status}] #{&1}")))
   end
 end
