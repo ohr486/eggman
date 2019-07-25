9ca8ba47ea1f3c6daff0939ba25c6e76b3b50246
ohr486 <ohr486@gmail.com>
2019-07-25 22:23:24 +0900

    bump ex ver

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Eggman.MixProject do
     [
       app: :eggman,
       version: "0.0.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
